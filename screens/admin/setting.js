653595ad3ead9695843ff4462d7c0a1191ba693f
Thế Nghĩa <nghiask2018@gmail.com>
2024-05-12 01:45:28 +0700

    feat: admin change password

---
+++ screens/admin/setting.js
@@ -0,0 +1,47 @@
+
+import { db } from "../../services/firebase.js";
+import { collection, doc, getDoc, updateDoc } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
+
+function hashPassword(password) {
+    return CryptoJS.SHA256(password).toString();
+}
+
+const form = document.querySelector('.reset-form');
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    // Lấy giá trị từ các trường input
+    const password = document.getElementById('password').value;
+    const hashedPassword = hashPassword(password);
+    const newPassword = document.getElementById('new-password').value;
+    const confirmPassword = document.getElementById('confirm-password').value;
+
+    // Kiểm tra xác thực mật khẩu cũ
+    const adminDocRef = doc(db, 'Admin', 'cw2aWQmtV0MIpDU8yq1Q');
+    const adminDocSnapshot = await getDoc(adminDocRef);
+    const adminData = adminDocSnapshot.data();
+
+    if (adminData.MatKhau !== hashedPassword) {
+        alert('Incorrect current password.');
+        return;
+    }
+
+    // Kiểm tra mật khẩu mới và mật khẩu xác nhận
+    if (newPassword !== confirmPassword) {
+        alert('New password and confirm password do not match.');
+        return;
+    }
+
+    // Cập nhật mật khẩu mới
+    try {
+        await updateDoc(adminDocRef, {
+            MatKhau: hashPassword(newPassword)
+        });
+
+        alert('Password updated successfully.');
+    } catch (error) {
+        console.error('Error updating password:', error);
+        alert('An error occurred while updating the password.');
+    }
+});
